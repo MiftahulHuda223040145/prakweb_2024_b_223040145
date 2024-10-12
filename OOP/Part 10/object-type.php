77167f550e70e4ab4b61203ea9a93be2dc2eacf3
Miftahul Huda <heromiftah2004@gmail.com>
2024-10-12 20:47:02 +0700

    adding part 9 - part 12

---
+++ OOP/Part 10/object-type.php
@@ -0,0 +1,38 @@
+<?php 
+
+class Produk {
+    public  $judul,
+            $penulis,
+            $penerbit,
+            $harga;
+
+    public function getLabel(){
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0){ //diterima = v
+        $this->judul = $judul; //lalu dipakai untuk menggantikan property nya ==
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+}
+
+class CetakInfoProduk {
+    public function cetak( Produk $produk ) {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000); //instansiasi (dibuat objek) dan dikirim ke constructor ^
+$produk2 = new Produk("Uncharted", "Neil Duckman", "Sony Computer", 250000);
+$produk3 = new Produk("Dragon Ball");
+
+echo "Komik: " . $produk1->getLabel();
+echo "<br>";
+echo "Game: " . $produk2->getLabel();
+echo "<br>";
+
+$infoProduk = new CetakInfoProduk();
+echo $infoProduk -> cetak($produk1);
