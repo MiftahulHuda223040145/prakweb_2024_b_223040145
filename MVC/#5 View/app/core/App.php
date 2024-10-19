9e6272808757a51626254809f1cd8bcd35c796d5
Miftahul Huda <heromiftah2004@gmail.com>
2024-10-19 10:56:27 +0700

    Modified #5 View

--- MVC/#5 View/app/core/App.php
+++ MVC/#5 View/app/core/App.php
@@ -8,8 +8,8 @@ class App {
     public function __construct() {
         $url = $this->parseURL();
         
-        //controller (error jika tidak dituliskan controllernya pada url)
-        if(file_exists('../app/controllers/' . $url[0] . '.php')) {
+        //controller (penambahan isset($url[0]) && karena error jika tidak ditambahkan controller)
+        if(isset($url[0]) && file_exists('../app/controllers/' . $url[0] . '.php')) {
             $this->controller = $url[0];
             unset($url[0]);
         }
